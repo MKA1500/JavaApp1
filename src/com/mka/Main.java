e63465ab12db843b6d3f1b392771eab8c8a83345
MKA1500 <maciejkabsch@gmail.com>
2019-07-16 21:24:20 +0200

    just checking

---
+++ src/com/mka/Main.java
@@ -0,0 +1,20 @@
+package com.mka;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte myAge = 30;
+        int viewsCount = 1_100_022_234;
+        long bigViewsCount = 5_100_022_234L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isOk = false;
+        Date now = new Date();
+
+        System.out.println(myAge);
+        System.out.println(viewsCount);
+        System.out.println(now);
+    }
+}
