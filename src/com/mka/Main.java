473c9a7d220705d4568355f90479c13c7fecbccd
MKA1500 <maciejkabsch@gmail.com>
2019-07-18 22:10:11 +0200

    some exercises

--- src/com/mka/Main.java
+++ src/com/mka/Main.java
@@ -1,5 +1,6 @@
 package com.mka;
 
+import java.awt.*;
 import java.util.Date;
 
 public class Main {
@@ -11,10 +12,20 @@ public class Main {
         float price = 10.99F;
         char letter = 'A';
         boolean isOk = false;
-        Date now = new Date();
 
-        System.out.println(myAge);
+        Date now = new Date();
+        Point point1 = new Point(1,1);
+        Point point2 = point1;
+        point2.x = 2;
+        String message = "Hello World" + "!";
+
+        System.out.println(point1);
         System.out.println(viewsCount);
         System.out.println(now);
+        System.out.println(message.endsWith("!"));
+        System.out.println(message.startsWith("!"));
+        System.out.println(message.length());
+        System.out.println(message.indexOf("o"));
+        System.out.println(message.replace("o", "y"));
     }
 }
