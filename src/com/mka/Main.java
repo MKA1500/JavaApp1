b5d58e15eac7c2ac361763956805a5a5f80d261f
MKA1500 <maciejkabsch@gmail.com>
2019-10-26 10:49:06 +0200

    playing with...

--- src/com/mka/Main.java
+++ src/com/mka/Main.java
@@ -1,31 +1,50 @@
 package com.mka;
 
-import java.awt.*;
-import java.util.Date;
+class Person {
+	private String name;
+	private int age;
+	
+	public Person() {
+		System.out.println("Hi, I'm new person. My name and age are not yet set.");
+	}
+	
+	public Person(int age, String name) {
+		this.setAge(age);
+		this.setName(name);	
+	}
+
+	public void setName(String newName) {
+		this.name = newName;
+	}
+	public void setAge(int newAge) {
+		this.age = newAge;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public void speak() {
+		System.out.println(this.name + " " + this.age);
+	}
+}
 
 public class Main {
-
-    public static void main(String[] args) {
-        byte myAge = 30;
-        int viewsCount = 1_100_022_234;
-        long bigViewsCount = 5_100_022_234L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isOk = false;
-
-        Date now = new Date();
-        Point point1 = new Point(1,1);
-        Point point2 = point1;
-        point2.x = 2;
-        String message = "Hello World" + "!";
-
-        System.out.println(point1);
-        System.out.println(viewsCount);
-        System.out.println(now);
-        System.out.println(message.endsWith("!"));
-        System.out.println(message.startsWith("!"));
-        System.out.println(message.length());
-        System.out.println(message.indexOf("o"));
-        System.out.println(message.replace("o", "y"));
-    }
+	public static void main(String[] args) {
+		Person person1 = new Person(24, "Maciej");	
+		Person person2 = new Person(284, "Marcin");
+		
+		person1.speak();
+		System.out.println(person1.getAge());
+		person2.speak();
+		System.out.println(person2.getAge());
+		
+		Person person3 = new Person();
+		
+		person3.setAge(21);
+		person3.setName("Nikodem");
+		person3.speak();
+	}
 }
